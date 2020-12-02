a54d0a42ddee28a4e2f9d07ba2912d7342edd1bd
Kaustubh-Dengle <dengle.kaustubh@gmail.com>
2020-12-02 15:42:55 +0530

    taking i/p and showing it in o/p

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate")
+var textInput = document.querySelector("#txt-input-area")
+
+
+function clickListener(){
+    console.log("clicked")
+    console.log("input", textInput.value)
+}
+
+btnTranslate.addEventListener("click", clickListener)
