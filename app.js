bad8dd9d0fba3087b326bdfc6ac10c0fcd7d7983
Kaustubh-Dengle <dengle.kaustubh@gmail.com>
2020-12-04 14:06:07 +0530

    app done, styling remaining

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate")
 var textInput = document.querySelector("#txt-input-area")
 var outputText = document.querySelector("#output")
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandling(error){
+    console.log("Error occured: ", error)
+}
+
 function clickListener(){
-    outputText.innerText = "translatedtext " + textInput.value
+    var inputTxt = textInput.value
+
+    fetch(getTranslationURL(inputTxt))
+    .then(res => res.json())
+    .then(translation => {
+        var translatedTxt = translation.contents.translated
+        outputText.innerText = translatedTxt
+    })
+    .catch(errorHandling)
+    
 }
 
 btnTranslate.addEventListener("click", clickListener)
